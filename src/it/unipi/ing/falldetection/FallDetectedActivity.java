8423ee02c6ac269cbc00186e3f939d4663750706
Antonio Macrì <ing.antonio.macri@gmail.com>
2014-02-09 15:18:11 +0100

    Remove menu from FallDetectedActivity.

--- src/it/unipi/ing/falldetection/FallDetectedActivity.java
+++ src/it/unipi/ing/falldetection/FallDetectedActivity.java
@@ -2,7 +2,6 @@ package it.unipi.ing.falldetection;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class FallDetectedActivity extends Activity
 {
@@ -10,12 +9,5 @@ public class FallDetectedActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fall_detected);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.fall_detected, menu);
-        return true;
     }
 }
